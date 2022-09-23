c72810fe29c2482fa04aa17c873567a4cba4ad91
Olaoye Mich <41102062+michson@users.noreply.github.com>
2022-09-23 06:14:03 +0100

    Update Index.aspx.cs

--- Online_Voting_System/Index.aspx.cs
+++ Online_Voting_System/Index.aspx.cs
@@ -9,7 +9,6 @@ using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
 
-
 public partial class Index : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
